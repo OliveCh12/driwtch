2696f10f0c7e43cc16097b7b00f9cdd537837ab4
Olivier <olivierchemla@gmail.com>
2021-01-07 16:31:57 +0100

    Remove imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 
 import { GlobalProvider } from "./GlobalContext";
 
